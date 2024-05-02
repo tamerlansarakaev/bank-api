2a2f0800d2821ee942819ce2b92ae69d48c804a1
Tamerlan Sarakaev <timasarakaev>
2024-05-02 17:02:22 +0300

    add entities

--- src/card/card.entity.ts
+++ src/card/card.entity.ts
@@ -1,19 +1,56 @@
-import { IsInt, IsNotEmpty, IsString, MinLength } from "class-validator";
-import { Column, PrimaryGeneratedColumn } from "typeorm";
+import { IsDate, IsInt, IsNotEmpty, IsString, Length, MinLength } from "class-validator";
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
 
+export enum cardStatus {
+    ACTIVE = 'Active',
+    BLOCKED = 'Blocked'
+}
+
+@Entity()
 export class Card {
     @PrimaryGeneratedColumn()
-    id: number
+    @Column({ unique: true })
+    id: number;
 
+    @Column()
     @IsString()
     @IsNotEmpty()
-    @MinLength(5, { message: "Full Name must have atleast 5 characters" })
+    @MinLength(5, { message: "Full Name must have at least 5 characters" })
     fullName: string;
 
+    @Column({ unique: true })
     @IsInt()
     @IsNotEmpty()
-    @MinLength(8, { message: "Full Name must have atleast 8 characters" })
-    @Column({ unique: true })
+    @MinLength(8, { message: "Card Number must have at least 8 digits" })
     @PrimaryGeneratedColumn()
-    cardNumber: number
+    cardNumber: number;
+
+    @Column()
+    @IsDate()
+    expiration_date: Date;
+
+    @Column()
+    @IsNotEmpty()
+    @Length(3, 3)
+    cvv: number;
+
+    @Column()
+    userId: number;
+
+    @Column()
+    balance: number;
+
+    @Column()
+    @IsInt()
+    currency: number;
+
+    @Column()
+    status: cardStatus.ACTIVE | cardStatus.BLOCKED
+
+    @Column()
+    statusMessage: string | null;
+
+    @Column()
+    transactions: Array<number> | null
+
 }
