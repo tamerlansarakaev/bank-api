5278d3bbc7dac59dbe8b87dc2f47a5230fcb065a
Khadzhi Sarakaev <sarakaev2098@mail.ru>
2024-05-03 17:28:45 +0300

    add user route, configured main.ts, add handle errors

--- src/user/user.entity.ts
+++ src/user/user.entity.ts
@@ -1,32 +1,30 @@
-import { IsAlphanumeric, IsEmail,  IsString, MinLength } from "class-validator";
-import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { IsAlphanumeric, IsEmail, IsString, MinLength } from 'class-validator';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity()
 export class User {
-    @PrimaryGeneratedColumn()
-    id: number;
+  @PrimaryGeneratedColumn()
+  id: number;
 
-    @Column()
-    @IsString()
-    @MinLength(3, { message: 'Name must have at least 3 characters' })
-    @IsAlphanumeric(null, { message: 'Name does not allow other than alphanumeric characters.' })
-    name: string;
+  @Column()
+  @IsString()
+  @MinLength(3, { message: 'Name must have at least 3 characters' })
+  name: string;
 
-    @Column()
-    @IsString()
-    @MinLength(3, { message: 'Surname must have at least 3 characters' })
-    @IsAlphanumeric(null, { message: 'Surname does not allow other than alphanumeric characters.' })
-    surname: string;
+  @Column()
+  @IsString()
+  @MinLength(3, { message: 'Surname must have at least 3 characters' })
+  surname: string;
 
-    @Column({ unique: true })
-    @IsEmail()
-    email: string;
+  @Column({ unique: true })
+  @IsEmail()
+  email: string;
 
-    @Column()
-    @IsString()
-    @MinLength(5, { message: "Password must have at least 5 characters" })
-    password: string;
+  @Column()
+  @IsString()
+  @MinLength(5, { message: 'Password must have at least 5 characters' })
+  password: string;
 
-    @Column({ type: 'jsonb', nullable: true })
-    cardList: number[];
+  @Column({ type: 'jsonb', nullable: true })
+  cardList: number[];
 }
