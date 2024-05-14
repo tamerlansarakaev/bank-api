c4edb411186f74bc1585b3e53d324bd5f25b4a74
tamerlansarakaev <timasarakaev@gmail.com>
2024-05-14 15:00:19 +0300

    improve code

--- src/client/controllers/transactions.controller.ts
+++ src/client/controllers/transactions.controller.ts
@@ -1,7 +1,9 @@
 import { Controller, Get } from '@nestjs/common';
+import { TransactionsService } from '../services/transactions.service';
 
 @Controller('client/transactions')
 export class TransactionsController {
+  constructor(transactionsService: TransactionsService) {}
   @Get()
   async getTransactions() {}
 }
